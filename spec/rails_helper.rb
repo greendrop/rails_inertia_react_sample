150d69fd143ebbb6055b1ce36216f5bfc349fe5b
greendrop <greendrop.devel@gmail.com>
2025-11-28 03:04:45 +0000

    chore: RSpecの設定でファイル位置に基づくスペックタイプの推論を有効化を既存コメント部分に変更

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -66,13 +66,10 @@ RSpec.configure do |config|
   # behaviour is considered legacy and will be removed in a future version.
   #
   # To enable this behaviour uncomment the line below.
-  # config.infer_spec_type_from_file_location!
+  config.infer_spec_type_from_file_location!
 
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-
-  # Enable automatic inference of spec types based on file location
-  config.infer_spec_type_from_file_location!
 end
