e101fa20b47fadf2cf71146d03ab665c0e87de3e
greendrop <greendrop.devel@gmail.com>
2025-11-29 23:20:01 +0000

    chore: ユーザー向けホームページのアプリケーションコントローラとレイアウトを実装

--- app/controllers/user_site/homes_controller.rb
+++ app/controllers/user_site/homes_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module UserSite
-  class HomesController < ApplicationController
+  class HomesController < UserSite::ApplicationController
     def show
       render inertia: {}
     end
