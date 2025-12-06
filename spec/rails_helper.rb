9399008d388cd4f40f030808dd3fa448c536c205
greendrop <greendrop.devel@gmail.com>
2025-12-06 21:08:20 +0000

    ci: Rubyテスト用のGitHub Actionsワークフローを追加
    Rubyのテストを実行するためのGitHub Actionsワークフローを新規作成しました。
    テストとカバレッジの収集を行う設定を含んでいます。

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+if ENV['COVERAGE']
+  require 'simplecov'
+  SimpleCov.start 'rails'
+end
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
