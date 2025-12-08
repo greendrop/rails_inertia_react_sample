4beb330bb1fb91315f3241422bb4e6fb88fed75e
greendrop <greendrop.devel@gmail.com>
2025-12-08 20:40:02 +0000

    style(biome.json, vite.config.ts, vitest.config.ts): コードフォーマットの統一

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,15 @@
-import { defineConfig, mergeConfig } from 'vitest/config'
-import viteConfig from './vite.config.ts'
+import { defineConfig, mergeConfig } from "vitest/config"
+import viteConfig from "./vite.config.ts"
 
-export default mergeConfig(viteConfig, defineConfig({
-  test: {
-    environment: 'happy-dom',
-    include: [
-      // ルートが`app/frontend`のため、親ディレクトリを指定する
-      '../../spec/frontend/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}',
-    ],
-  },
-}))
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      environment: "happy-dom",
+      include: [
+        // ルートが`app/frontend`のため、親ディレクトリを指定する
+        "../../spec/frontend/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}",
+      ],
+    },
+  }),
+)
