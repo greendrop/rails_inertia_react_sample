8228162847e16fb9861533fc012559461c322c36
greendrop <greendrop.devel@gmail.com>
2025-12-08 11:57:29 +0000

    feat(deps): jsdomを追加し、テスト環境を設定
    package.jsonにjsdomを追加し、vitest.config.tsでテスト環境をjsdomに設定しました。

---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig, mergeConfig } from 'vitest/config'
+import viteConfig from './vite.config.ts'
+
+export default mergeConfig(viteConfig, defineConfig({
+  test: {
+    environment: 'jsdom',
+    include: [
+      '../../spec/frontend/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}',
+    ],
+  },
+}))
