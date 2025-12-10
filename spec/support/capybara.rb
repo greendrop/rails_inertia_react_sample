22d70085cf95afdd838baecad1116f2a47754d3d
greendrop <greendrop.devel@gmail.com>
2025-12-10 03:47:57 +0000

    feat(tests): Playwrightを使用したシステムテストの追加
    CapybaraとPlaywrightを統合し、記事一覧表示のテストを実装しました。

---
+++ spec/support/capybara.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'capybara/rspec'
+require 'capybara-playwright-driver'
+
+module PlaywrightHepler
+  def playwright_driver_options
+    { headless: true, browser_type: :chromium }
+  end
+end
+
+RSpec.configure do |config|
+  config.include PlaywrightHepler, type: :system
+
+  config.before(:suite) do
+    if ENV['CI'].blank?
+      # NOTE: playwrightでブラウザがインストールされているか確認し、インストールされていない場合はインストールする
+      dry_run_result = ''
+      IO.popen(['npx', 'playwright', 'install', '--dry-run', 'chromium'], err: %i[child out]) do |io|
+        dry_run_result = io.read
+      end
+      matched = dry_run_result.match(/Install location:\s*(.*)\s*$/)
+      install_location = matched[1] if matched
+
+      system('npx playwright install --with-deps chromium') if install_location.nil? || !Dir.exist?(install_location)
+    end
+  end
+
+  config.before(type: :system, js: true) do
+    driven_by(:playwright, options: playwright_driver_options)
+  end
+
+  config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
+
+  config.before(:each, :js, type: :system) do
+    driven_by(:playwright, options: playwright_driver_options)
+    Capybara.default_max_wait_time = 15
+  end
+end
---
+++ spec/system/admin_site/articles/index_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'AdminSite::Articles#index', :js do
+  context '記事が存在する場合' do
+    let!(:article1) { create(:article, title: 'First Article') }
+    let!(:article2) { create(:article, title: 'Second Article') }
+
+    before do
+      visit admin_site_articles_path
+    end
+
+    it '記事一覧が表示されること' do
+      within 'main' do
+        expect(page).to have_content('First Article')
+        expect(page).to have_content('Second Article')
+      end
+    end
+  end
+end
