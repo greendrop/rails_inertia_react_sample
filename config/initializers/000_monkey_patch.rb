e50454f5c791c35c8614bf97d46c85b69d0752ed
greendrop <greendrop.devel@gmail.com>
2025-12-22 11:18:00 +0000

    refactor(initializers): 先読みのためのコメントを追加

--- config/initializers/000_monkey_patch.rb
+++ config/initializers/000_monkey_patch.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# NOTE: ファイル名に `000_` プレフィックスを付与することで、この initializer を他の initializer よりも先に読み込むようにしています。
+#   lib 配下の monkey patch は、アプリケーションや他の initializer から利用されるクラス／メソッドの挙動を書き換えるため、依存コードより前に読み込む必要があります。
+
 Rails.root.glob('lib/monkey_patch/**/*.rb').sort.each do |file|
   require file
 end
