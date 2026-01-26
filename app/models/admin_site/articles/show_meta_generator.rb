2d8049d3a70f5c74b327a71b910e50f28bed1c11
greendrop <greendrop.devel@gmail.com>
2026-01-26 21:07:00 +0000

    feat(i18n): 記事詳細ページの国際化対応を追加

--- app/models/admin_site/articles/show_meta_generator.rb
+++ app/models/admin_site/articles/show_meta_generator.rb
@@ -7,7 +7,10 @@ module AdminSite
 
       def call
         [
-          { title: '記事詳細 | Rails Inertia React Sample - Admin Site' }
+          { title:
+          "#{
+            I18n.t('admin_site.general.resource_show_title', resource: Article.model_name.human)
+          } | Rails Inertia React Sample - Admin Site" }
         ]
       end
     end
--- app/models/admin_site/articles/show_props_generator.rb
+++ app/models/admin_site/articles/show_props_generator.rb
@@ -29,15 +29,24 @@ module AdminSite
       attr_reader :article
 
       def prop_page_header_title
-        '記事詳細'
+        I18n.t('admin_site.general.resource_show_title', resource: Article.model_name.human)
       end
 
       def prop_breadcrumb
         {
           items: [
-            { key: 'homes#show', label: 'ホーム', href: admin_site_root_path, isActive: false },
-            { key: 'articles#index', label: '記事一覧', href: admin_site_articles_path, isActive: false },
-            { key: 'articles#show', label: '記事詳細', href: admin_site_article_path(id: article.id), isActive: true }
+            { key: 'homes#show',
+              label: I18n.t('admin_site.general.home'),
+              href: admin_site_root_path,
+              isActive: false },
+            { key: 'articles#index',
+              label: I18n.t('admin_site.general.resource_index_title', resource: Article.model_name.human),
+              href: admin_site_articles_path,
+              isActive: false },
+            { key: 'articles#show',
+              label: I18n.t('admin_site.general.resource_show_title', resource: Article.model_name.human),
+              href: admin_site_article_path(id: article.id),
+              isActive: true }
           ]
         }
       end
@@ -55,12 +64,12 @@ module AdminSite
 
       def prop_article_field_names
         {
-          id: 'ID',
-          title: 'タイトル',
-          status: 'ステータス',
-          body: '本文',
-          createdAt: '作成日時',
-          updatedAt: '更新日時'
+          id: Article.human_attribute_name('id'),
+          title: Article.human_attribute_name('title'),
+          status: Article.human_attribute_name('status'),
+          body: Article.human_attribute_name('body'),
+          createdAt: Article.human_attribute_name('created_at'),
+          updatedAt: Article.human_attribute_name('updated_at')
         }
       end
 
