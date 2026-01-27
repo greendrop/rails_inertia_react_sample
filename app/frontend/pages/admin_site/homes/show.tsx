e3c573a135121bba35f6f2e938de896ee01e1cef
greendrop <greendrop.devel@gmail.com>
2026-01-27 11:36:19 +0000

    feat(homes): Showコンポーネントにページヘッダーとフラッシュアラートを追加

--- app/frontend/pages/admin_site/homes/show.tsx
+++ app/frontend/pages/admin_site/homes/show.tsx
@@ -1,8 +1,32 @@
 import type { ReactElement } from "react"
+import FlashAlert from "@/components/admin_site/shared/FlashAlert"
 import Layout from "@/components/admin_site/shared/Layout"
+import MetaTags from "@/components/admin_site/shared/MetaTags"
+import PageHeader from "@/components/admin_site/shared/PageHeader"
+import PageHeaderTitle from "@/components/admin_site/shared/PageHeaderTitle"
+import PageHeaderTitleText from "@/components/admin_site/shared/PageHeaderTitleText"
 
-export default function Show() {
-  return <div />
+type ShowProps = {
+  pageHeaderTitle: string
+}
+
+export default function Show({ pageHeaderTitle }: ShowProps) {
+  return (
+    <>
+      <MetaTags />
+      <div className="mb-4">
+        <PageHeader>
+          <PageHeaderTitle>
+            <PageHeaderTitleText>{pageHeaderTitle}</PageHeaderTitleText>
+          </PageHeaderTitle>
+        </PageHeader>
+      </div>
+
+      <div className="mb-4">
+        <FlashAlert />
+      </div>
+    </>
+  )
 }
 
 Show.layout = (page: ReactElement) => <Layout>{page}</Layout>
