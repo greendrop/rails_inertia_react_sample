0212146b6987cb62a303f05326ea004c43ee0ea9
greendrop <greendrop.devel@gmail.com>
2026-01-28 21:27:50 +0000

    test(admin_site/articles): 記事作成および更新のテストを整理

--- spec/requests/admin_site/articles/create_spec.rb
+++ spec/requests/admin_site/articles/create_spec.rb
@@ -4,41 +4,53 @@ require 'rails_helper'
 
 RSpec.describe 'AdminSite::Articles create' do
   describe 'POST /admin/articles', :inertia do
-    let!(:valid_params) do
-      {
-        article: {
-          title: '新しい記事',
-          body: '本文',
-          status: 'draft',
-          published_at: ''
-        }
-      }
-    end
-
-    it '有効なパラメータなら記事を作成して一覧へリダイレクトする' do
-      expect { post '/admin/articles', params: valid_params }.to change(Article, :count).by(1)
-      expect(response).to redirect_to("/admin/articles/#{Article.last.id}")
-      expect(flash[:notice]).to eq('記事が作成されました。')
-    end
-
-    it '無効なパラメータなら422とエラー情報を返す' do
-      invalid_params = valid_params.deep_dup
-      invalid_params[:article][:title] = ''
-
-      expect { post '/admin/articles', params: invalid_params }.not_to change(Article, :count)
-
-      expect(response).to have_http_status(:unprocessable_content)
-
-      expect(inertia).to render_component('admin_site/articles/new')
-      expect(inertia.props[:errors]).to eq({ title: ['タイトルを入力してください'] })
-      expect(inertia.props[:form]).to eq(
+    context '有効なパラメーターの場合' do
+      let!(:valid_params) do
         {
-          title: '',
-          body: '本文',
-          status: 'draft',
-          publishedAt: ''
+          article: {
+            title: '新しい記事',
+            body: '本文',
+            status: 'draft',
+            published_at: ''
+          }
         }
-      )
+      end
+
+      it '記事を作成して一覧へリダイレクトする' do
+        expect { post '/admin/articles', params: valid_params }.to change(Article, :count).by(1)
+        expect(response).to redirect_to("/admin/articles/#{Article.last.id}")
+        expect(flash[:notice]).to eq('記事が作成されました。')
+      end
+    end
+
+    context '無効なパラメーターの場合' do
+      let!(:invalid_params) do
+        {
+          article: {
+            title: '',
+            body: '本文',
+            status: 'draft',
+            published_at: ''
+          }
+        }
+      end
+
+      it '422とエラー情報を返す' do
+        expect { post '/admin/articles', params: invalid_params }.not_to change(Article, :count)
+
+        expect(response).to have_http_status(:unprocessable_content)
+
+        expect(inertia).to render_component('admin_site/articles/new')
+        expect(inertia.props[:errors]).to eq({ title: ['タイトルを入力してください'] })
+        expect(inertia.props[:form]).to eq(
+          {
+            title: '',
+            body: '本文',
+            status: 'draft',
+            publishedAt: ''
+          }
+        )
+      end
     end
   end
 end
--- spec/requests/admin_site/articles/update_spec.rb
+++ spec/requests/admin_site/articles/update_spec.rb
@@ -5,53 +5,68 @@ require 'rails_helper'
 RSpec.describe 'AdminSite::Articles update' do
   describe 'PATCH /admin/articles/:id', :inertia do
     let!(:article) { create(:article) }
-    let!(:valid_params) do
-      {
-        article: {
-          title: '更新後のタイトル',
-          body: '更新後の本文',
-          status: 'published',
-          published_at: '2025-12-31T12:00'
-        }
-      }
-    end
 
-    it '有効なパラメータなら記事を更新してリダイレクトする' do
-      patch "/admin/articles/#{article.id}", params: valid_params
-
-      expect(response).to redirect_to("/admin/articles/#{article.id}")
-      expect(flash[:notice]).to eq('記事が更新されました。')
-
-      article.reload
-      expect(article.title).to eq('更新後のタイトル')
-      expect(article.body).to eq('更新後の本文')
-      expect(article.status).to eq('published')
-      expect(article.published_at).to eq(Time.zone.parse('2025-12-31T12:00'))
-    end
-
-    it '無効なパラメータなら422とエラー情報を返す' do
-      invalid_params = valid_params.deep_dup
-      invalid_params[:article][:title] = ''
-
-      patch "/admin/articles/#{article.id}", params: invalid_params
-
-      expect(response).to have_http_status(:unprocessable_content)
-
-      expect(inertia).to render_component('admin_site/articles/edit')
-      expect(inertia.props[:errors]).to eq({ title: ['タイトルを入力してください'] })
-      expect(inertia.props[:form]).to eq(
+    context '有効なパラメーターの場合' do
+      let!(:valid_params) do
         {
-          title: '',
-          body: '更新後の本文',
-          status: 'published',
-          publishedAt: '2025-12-31T12:00'
+          article: {
+            title: '更新後のタイトル',
+            body: '更新後の本文',
+            status: 'published',
+            published_at: '2025-12-31T12:00'
+          }
         }
-      )
+      end
+
+      it '記事を更新してリダイレクトする' do
+        patch "/admin/articles/#{article.id}", params: valid_params
+
+        expect(response).to redirect_to("/admin/articles/#{article.id}")
+        expect(flash[:notice]).to eq('記事が更新されました。')
+
+        article.reload
+        expect(article.title).to eq('更新後のタイトル')
+        expect(article.body).to eq('更新後の本文')
+        expect(article.status).to eq('published')
+        expect(article.published_at).to eq(Time.zone.parse('2025-12-31T12:00'))
+      end
     end
 
-    it '存在しないIDでは404を返す' do
-      patch "/admin/articles/#{article.id + 999_999}", params: valid_params
-      expect(response).to have_http_status(:not_found)
+    context '無効なパラメーターの場合' do
+      let!(:invalid_params) do
+        {
+          article: {
+            title: '',
+            body: '更新後の本文',
+            status: 'published',
+            published_at: '2025-12-31T12:00'
+          }
+        }
+      end
+
+      it '422とエラー情報を返す' do
+        patch "/admin/articles/#{article.id}", params: invalid_params
+
+        expect(response).to have_http_status(:unprocessable_content)
+
+        expect(inertia).to render_component('admin_site/articles/edit')
+        expect(inertia.props[:errors]).to eq({ title: ['タイトルを入力してください'] })
+        expect(inertia.props[:form]).to eq(
+          {
+            title: '',
+            body: '更新後の本文',
+            status: 'published',
+            publishedAt: '2025-12-31T12:00'
+          }
+        )
+      end
+    end
+
+    context '存在しない記事IDの場合' do
+      it '404を返す' do
+        patch "/admin/articles/#{article.id + 999_999}"
+        expect(response).to have_http_status(:not_found)
+      end
     end
   end
 end
